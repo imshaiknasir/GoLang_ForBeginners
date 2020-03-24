6c14633d5290f49d7e443cd12a3fb1f8f4f2b0dc
imshaiknasir <skfiroj1994@gmail.com>
2020-03-25 02:57:00 +0530

    Added Vaiables topic

--- basics_01/02_Variables.go
+++ basics_01/02_Variables.go
@@ -1,1 +1,30 @@
 package main
+
+import "fmt"
+
+func main() {
+
+	// Simple way of declating a variable
+	var ironman string = "I am Ironman"
+	fmt.Println(ironman)
+
+	// Declaring variables using colon symbol (it's useful when the data-type is unclear to us)
+	superman := "I am Superman"
+	fmt.Println(superman)
+
+	//knowing the data-type the has been assigned to variable uning "Printf"
+	spiderman := "I am Spiderman"
+	fmt.Printf("%v, %T", spiderman, spiderman) // "%v" will pass the value, & "%T" will show the data-type (like string/interger/etc..)
+
+	//declaring multiple variable
+	batman, joker := "I am Batman", "I am Joker"
+	fmt.Println(batman, joker)
+
+	// Declaring multiple variables unsing variable as a function
+	var (
+		wonderWoman = "I'm wonderWoman"
+		health      = 100
+		speed       = 750.86
+	)
+	fmt.Println(wonderWoman, health, speed)
+}
