98d9cec1d4c8ce94dbf5b01b4dc0545d72c54a12
imshaiknasir <skfiroj1994@gmail.com>
2020-03-25 02:10:25 +0530

    Initail code-push to github

---
+++ basics_01/01_HelloWorld.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+//Example-1
+
+// func main() {
+// 	fmt.Println("Hello, world")
+// }
+
+//Example-2
+
+func main() {
+	var something int = 50
+	fmt.Println(something)
+}
+
+//Example-3
+
+// func main() {
+// 	var name string = "Tom"
+// 	fmt.Println("Hello")
+// }
